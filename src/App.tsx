ef5f9f6325a143ebfb1cfe3f5702d8899c41cc96
Chijex5 <138696001+Chijex5@users.noreply.github.com>
2025-02-13 01:43:29 +0100

    [Magic Patterns] Updated from 'Student Calendar Planner'

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Calendar } from "./components/Calendar";
+export function App() {
+  return <main className="min-h-screen bg-gray-50 p-4">
+      <Calendar />
+    </main>;
+}
